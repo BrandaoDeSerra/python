a7c8c2b7d925c6bdb51f9188eb6bd8e8ccf6187d
Brandão <alexandre.magalhaes@mv.com.br>
2019-11-01 15:11:57 -0300

    Ajuste teste

--- Exercicio IA/exercicio02.py
+++ Exercicio IA/exercicio02.py
@@ -5,6 +5,20 @@ Execute o algoritmo com 3 variações de parâmetros e plote o gráfico de evolu
 Obs: Nesse problema, é preciso primeiro definir como representar um cromossomo e como definir uma função de aptidão.
 https://github.com/victorouttes/evolutionary-computing-01/blob/master/caixeiro-viajante.ipynb
 
+int numeroDeDezenasProJogo = 8;
+		int[] arrayDezenasDisponiveis = {1,3,5,7,9,11,13,15,17,19,21,23,25};
 
+	    int[] jogo = new int[numeroDeDezenasProJogo];
+
+		Neuronios.neuronioDeCombinacao(arrayDezenasDisponiveis , numeroDeDezenasProJogo-1, 0, jogo,jogosPossiveis);
+
+		for (int[] jogoPre : jogosPossiveis)
 
 '''
+distancias = ['1221=2','1331=6','1441=3','2332=4','2442=7','3443=2']
+cidades = [1,2,3,4]
+
+11
+10
+01
+00
