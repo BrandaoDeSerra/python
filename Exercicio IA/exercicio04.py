ac3eed4a916c780d1009bea48343b9f18e436ae3
Brandao <brandaodeserra@hotmail.com>
2019-11-30 21:01:08 -0300

    Conclusao do algoritimo exercicio 01 inclusão 02

--- Exercicio IA/exercicio04.py
+++ Exercicio IA/exercicio04.py
@@ -8,99 +8,169 @@ R1
 v1 = w * v1 + c1 * r1 (p1 - s1) +
 v1 = 0.9 * 3
 
+
+
+
 Usando alguma biblioteca ou implementando diretamente em alguma linguagem de programação,
 desenvolva uma solução para minimizar a função de Rosenbrock (em anexo). Faça testes pra
 identificar um conjunto de parâmetros adequado. Plote o gráfico de evolução da solução.
 
+Xmin / Ymin = -2
+Xmax / Ymax = +2
+
+minGlobal = +1
 '''
+#!/usr/bin/env python
+
+################################################################################
+# File name: pso.py                                                            #
+# Description: A module that solves a minimization problem using PSO           #
+#------------------------------------------------------------------------------#
+# Author: Hussein S. Al-Olimat                                                 #
+# Email: hussein@knoesis.org                                                   #
+#------------------------------------------------------------------------------#
+# Date-last-modified: Nov 7, 2016                                              #
+# Python-version: 2.7                                                          #
+################################################################################
+# This file is part of hussein.space: Hussein S. Al-Olimat's weblog -          #
+# Blogging about computer science things I've done or intesetred in            #
+#------------------------------------------------------------------------------#
+# This code is licensed under a Creative Commons Attribution 4.0 International #
+# License. The full license terms and conditions can be found over:            #
+# https://creativecommons.org/licenses/by/4.0/                                 #
+################################################################################
+
+
 import random
-import numpy as np
+import sys
 
-W = 0.5
-c1 = 0.8
-c2 = 0.9
+################################################################################
 
-n_iterations = int(input("Inform the number of iterations: "))
-target_error = float(input("Inform the target error: "))
-n_particles = int(input("Inform the number of particles: "))
+# the x and y in our function (x - y + 7) (aka. dimensions)
+number_of_variables = 2
+# the minimum possible value x or y can take
+min_value = -2
+# the maximum possible value x or y can take
+max_value = 2
+# the number of particles in the swarm
+number_of_particles = 10
+# number of times the algorithm moves each particle in the problem space
+number_of_iterations = 2000
 
+w = 0.729    # inertia
+c1 = 1.49 # cognitive (particle)
+c2 = 1.49 # social (swarm)
 
-def fncRosenbrock(particle):
+################################################################################
+
+class Particle:
+
+    # this is only done one time when we create each particle
+    # value of positions, velocities, fitness, best fitness, and best positions
+    # is going to be updated later in the code during each iteration
+    def __init__(self, number_of_variables, min_value, max_value):
+
+        # init x and y values
+        self.positions = [0.0 for v in range(number_of_variables)]
+        # init velocities of x and y
+        self.velocities = [0.0 for v in range(number_of_variables)]
+
+        for v in range(number_of_variables):
+            # update x and y positions
+            self.positions[v] = ((max_value - min_value) * random.random()
+                                + min_value)
+            # update x and y velocities
+            self.velocities[v] = ((max_value - min_value) * random.random()
+                                + min_value)
+
+        # current fitness after updating the x and y values
+        self.fitness = Fitness(self.positions)
+        # the current particle positions as the best fitness found yet
+        self.best_particle_positions = list(self.positions)
+        # the current particle fitness as the best fitness found yet
+        self.best_particle_fitness = self.fitness
+
+#Rosenbrock
+def Fitness(positions):
     x = particle[0]
     y = particle[1]
     a = 1. - x
     b = y - x * x
     return (b * b * 100.) + (a * a)
 
-class Particle():
-    def __init__(self):
-        self.position = np.array([(-1) ** (bool(random.getrandbits(1))) * random.random()*50, (-1)**(bool(random.getrandbits(1))) * random.random()*50])
-        self.pbest_position = self.position
-        self.pbest_value = float('inf')
-        self.velocity = np.array([0,0])
+# calculate a new velocity for one variable
+def calculate_new_velocity_value(particle, v):
 
-    def __str__(self):
-        print("I am at ", self.position, " meu pbest is ", self.pbest_position)
+    # generate random numbers
+    r1 = random.random()
+    r2 = random.random()
 
-    def move(self):
-        self.position = self.position + self.velocity
+    # the learning rate part
+    part_1 = (w * particle.velocities[v])
+    # the cognitive part - learning from itself
+    part_2 = (c1 * r1 * (particle.best_particle_positions[v] - particle.positions[v]))
+    # the social part - learning from others
+    part_3 = (c2 * r2 * (best_swarm_positions[v] - particle.positions[v]))
+
+    new_velocity = part_1 + part_2 + part_3
+
+    return new_velocity
+
+################################################################################
+
+# create the swarm
+swarm = [Particle(number_of_variables, min_value, max_value)
+                    for __x in range(number_of_particles)]
+
+######################################### best particle error and positions ####
+
+best_swarm_positions = [0.0 for v in range(number_of_variables)]
+best_swarm_fitness = sys.float_info.max
+
+for particle in swarm: # check each particle
+    if particle.fitness < best_swarm_fitness:
+        best_swarm_fitness = particle.fitness
+        best_swarm_positions = list(particle.positions)
+
+################################################################################
+
+for __x in range(number_of_iterations):
+    for particle in swarm:
+        # start moving/updating particles to calculate new fitness
+
+        # compute new velocities for each particle
+        for v in range(number_of_variables):
+
+            particle.velocities[v] = calculate_new_velocity_value(particle, v)
+
+            if particle.velocities[v] < min_value:
+                particle.velocities[v] = min_value
+            elif particle.velocities[v] > max_value:
+                particle.velocities[v] = max_value
+
+        # compute new positions using the new velocities
+        for v in range(number_of_variables):
+            particle.positions[v] += particle.velocities[v]
+
+            if particle.positions[v] < min_value:
+                particle.positions[v] = min_value
+            elif particle.positions[v] > max_value:
+                particle.positions[v] = max_value
+
+        # compute the fitness of the new positions
+        particle.fitness = Fitness(particle.positions)
+
+        # are the new positions a new best for the particle?
+        if particle.fitness < particle.best_particle_fitness:
+            particle.best_particle_fitness = particle.fitness
+            particle.best_particle_positions = list(particle.positions)
+
+        # are the new positions a new best overall?
+        if particle.fitness < best_swarm_fitness:
+            best_swarm_fitness = particle.fitness
+            best_swarm_positions = list(particle.positions)
 
 
-class Space():
+################################################################################
 
-    def __init__(self, target, target_error, n_particles):
-        self.target = target
-        self.target_error = target_error
-        self.n_particles = n_particles
-        self.particles = []
-        self.gbest_value = float('inf')
-        self.gbest_position = np.array([random.random()*50, random.random()*50])
-
-    def print_particles(self):
-        for particle in self.particles:
-            particle.__str__()
-
-    def fitness(self, particle):
-        return fncRosenbrock(particle)
-
-    def set_pbest(self):
-        for particle in self.particles:
-            fitness_cadidate = self.fitness(particle)
-            if(particle.pbest_value > fitness_cadidate):
-                particle.pbest_value = fitness_cadidate
-                particle.pbest_position = particle.position
-
-
-    def set_gbest(self):
-        for particle in self.particles:
-            best_fitness_cadidate = self.fitness(particle)
-            if(self.gbest_value > best_fitness_cadidate):
-                self.gbest_value = best_fitness_cadidate
-                self.gbest_position = particle.position
-
-    def move_particles(self):
-        for particle in self.particles:
-            global W
-            new_velocity = (W*particle.velocity) + (c1*random.random()) * (particle.pbest_position - particle.position) + \
-                           (random.random()*c2) * (self.gbest_position - particle.position)
-            particle.velocity = new_velocity
-            particle.move()
-
-
-search_space = Space(1, target_error, n_particles)
-particles_vector = [Particle() for _ in range(search_space.n_particles)]
-search_space.particles = particles_vector
-search_space.print_particles()
-
-iteration = 0
-while(iteration < n_iterations):
-    search_space.set_pbest()
-    search_space.set_gbest()
-
-    if(abs(search_space.gbest_value - search_space.target) <= search_space.target_error):
-        break
-
-    search_space.move_particles()
-    iteration += 1
-
-print("The best solution is: ", search_space.gbest_position, " in n_iterations: ", iteration)
+print(best_swarm_positions)
