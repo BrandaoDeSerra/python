ae94ff0851e69079037247baccbd0eadca2a8ed6
Brandao <brandaodeserra@hotmail.com>
2020-03-14 08:28:53 -0300

    Conclusão

---
+++ Exercicio IA/VisaoComputacional/aula_2.py
@@ -0,0 +1,27 @@
+import cv2 as cv
+from matplotlib import pyplot as plt
+import numpy as np
+
+img = cv.imread('./arquivos/goat.jpeg')
+
+base = list(range(256))
+for i in base:
+    base[i] = 0
+
+img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+coluna = img_gray.shape[0]
+linha  = img_gray.shape[1]
+for y in range(coluna):
+    for x in range(linha):
+        base[img_gray[y, x]] = base[img_gray[y, x]] + 1
+
+plt.bar(list(range(256)),base)
+plt.ylabel('Count')
+plt.xlabel('Cor')
+plt.title('Histogram count gray scale')
+plt.ylim(0, 2000)
+plt.xlim(0, 255)
+plt.show()
+
+plt.hist(img_gray.ravel(),256,[0,256])
+plt.show()
--- Exercicio IA/VisaoComputacional/exemplo_02.py
+++ Exercicio IA/VisaoComputacional/exemplo_02.py
@@ -4,25 +4,22 @@ import cv2
 cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier('arquivos/haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('arquivos/haarcascade_eye.xml')
-smile_cascade = cv2.CascadeClassifier('arquivos/haarcascade_smile.xml')
 
 while(True):
-	_, frame = cap.read()
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	conectado, frame = cap.read()
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #tranforma a imagem em preto e branco
+	# cv2.imshow('gray', gray)
 	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 	for (x,y,w,h) in faces:
-		cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+		cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
 		roi_gray = gray[y:y+h, x:x+w]
+		#cv2.imshow('roi_gray', roi_gray)
 		roi_color = frame[y:y+h, x:x+w]
 		eyes = eye_cascade.detectMultiScale(roi_gray)
 		for (ex,ey,ew,eh) in eyes:
-			cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+			cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(255,0,0),2)
 
-		#smile = smile_cascade.detectMultiScale(gray, 1.3, 5)
-		#for (sx,sy,sw,sh) in smile:
-			#cv2.rectangle(roi_color,(sx,sy),(sx+sw,sy+sh),(0,255,255),2)
-
-	cv2.imshow('frame',frame)
+	cv2.imshow('Visao Computacional',frame)
 
 	key = cv2.waitKey(1)
 
