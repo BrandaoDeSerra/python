8bfb60c338ce795e1702ff658f2a947c97057375
Brandao <brandaodeserra@hotmail.com>
2020-03-14 15:48:52 -0300

    Conclusão

--- Exercicio IA/VisaoComputacional/aula_2.py
+++ Exercicio IA/VisaoComputacional/aula_2.py
@@ -1,6 +1,25 @@
 import cv2 as cv
-from matplotlib import pyplot as plt
-import numpy as np
+import matplotlib.pyplot as plt
+from skimage.io import imread
+from skimage.filters import roberts, sobel, scharr, prewitt
+
+
+img = imread("./arquivos/goat.jpeg", as_gray=True) # carregando imagem
+op_sobel = sobel(img)
+fig, axes = plt.subplots(nrows=2, ncols=2, sharex=True, sharey=True, figsize=(50, 50))
+ax = axes.ravel()
+ax[0].imshow(op_sobel, cmap=plt.cm.gray)
+ax[0].set_title('Operador de Sobel')
+
+for a in ax:
+    a.axis('off')
+
+plt.tight_layout()
+plt.show()
+
+
+
+
 
 img = cv.imread('./arquivos/goat.jpeg')
 
@@ -15,7 +34,7 @@ for y in range(coluna):
     for x in range(linha):
         base[img_gray[y, x]] = base[img_gray[y, x]] + 1
 
-plt.bar(list(range(256)), base )
+plt.bar(list(range(256)),base)
 plt.ylabel('Count')
 plt.xlabel('Cor')
 plt.title('Histogram count gray scale')
@@ -23,5 +42,4 @@ plt.ylim(0, 2000)
 plt.xlim(0, 255)
 plt.show()
 
-plt.hist(img_gray.ravel(),256,[0,256])
-plt.show()
+
---
+++ Exercicio IA/VisaoComputacional/filtro_bordas.py
@@ -0,0 +1,49 @@
+import numpy as np
+import cv2
+
+
+# função para fazer a correlação simples, movendo a máscara sobre a imagem
+def correlacao(img, img_corr, mascara):
+    # Percorre cada pixel da imagem
+    for coluna in range(img.shape[0]):
+        for linha in range(img.shape[1]):
+            try:
+                # primeira coluna
+                m = img[coluna - 1][linha + 1] * mascara[0][0]
+                m += img[coluna - 1][linha] * mascara[1][0]
+                m += img[coluna - 1][linha - 1] * mascara[2][0]
+
+                # Segunda coluna
+                m += img[coluna][linha + 1] * mascara[0][1]
+                m += img[coluna][linha] * mascara[1][1]
+                m += img[coluna][linha - 1] * mascara[2][1]
+
+                # Terceira coluna
+                m += img[coluna + 1][linha + 1] * mascara[0][2]
+                m += img[coluna + 1][linha] * mascara[1][2]
+                m += img[coluna + 1][linha - 1] * mascara[2][2]
+
+                # Faz a média dos valores e guarda como intensidade do pixel
+                img_corr[coluna][linha] = m / 9
+            except:
+                continue
+    return img_corr
+
+
+# Carregando imagem num vetor
+img = cv2.imread('./arquivos/goat.jpeg')
+img_corr = cv2.imread('./arquivos/goat.jpeg')
+
+# Cria uma máscara
+mascara = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+
+# Executa a função de correlação
+img_corr_comum = correlacao(img, img_corr, mascara)
+
+# Mostrar imagem
+cv2.imshow('Original', img)
+cv2.imshow('Sobel', img_corr_comum)
+
+# Funções para funcionamento correto ao mostrar a imagem numa janela
+cv2.waitKey(0)
+cv2.destroyAllWindows()
