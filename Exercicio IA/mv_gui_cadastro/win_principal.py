09ebde8faaa864ef42d76845a74969b6700b522d
Brandao <brandaodeserra@hotmail.com>
2020-05-10 18:01:54 -0300

    Conclusão

---
+++ Exercicio IA/mv_gui_cadastro/win_principal.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+from PIL import Image
+from PIL import ImageTk
+import tkinter as tki
+import threading
+import datetime
+import cv2
+import os
+
+janela = tki.Tk()
+janela.title("Cadastro de Colaboradores")
+janela.geometry("1280x768")
+
+
+
+
+janela.mainloop()
