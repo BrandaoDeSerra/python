da92cfc55b4dba78c6c8e570137a3ea27c649585
Brandao <brandaodeserra@hotmail.com>
2020-05-25 18:23:42 -0300

    Conclusão

--- Exercicio IA/VisaoComputacional/modulo_rastreamento/win_principal.py
+++ Exercicio IA/VisaoComputacional/modulo_rastreamento/win_principal.py
@@ -9,7 +9,10 @@ import requests
 from flask import json
 from base64 import b64encode
 import configparser
-from pygame import mixer
+import os
+from gtts import gTTS
+from datetime import datetime
+from pygame import mixer  # Load the popular external library
 
 
 class App:
@@ -18,8 +21,10 @@ class App:
         self.window.title(window_title)
         self.pwidth = width  # 320
         self.pheight = height  # 240
+        self.resetTimeout = int(0)
+        self.timeRecognition = int(5)
         faceCrop = ""
-        #FullScreenApp(self.window)
+        # FullScreenApp(self.window)
 
         # open video source (by default this will try to open the computer webcam)
         self.vid = MyVideoCapture(0)
@@ -41,14 +46,23 @@ class App:
 
     def update(self):
         # Get a frame from the video source
-        ret, frame = self.vid.get_frame(self.pwidth, self.pheight)
-        ret1, frame1 = self.vid1.get_frame_termal(self.pwidth, self.pheight)
-        if ret:
-            self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
-            self.canvas.create_image(0, 0, image=self.photo, anchor=tkinter.NW)
-        if ret1:
-            self.photo1 = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame1))
-            self.canvas1.create_image(0, 0, image=self.photo1, anchor=tkinter.NW)
+        if self.resetTimeout <= 0:
+            ret, frame, self.resetTimeout,self.timeRecognition = self.vid.get_frame(self.pwidth, self.pheight, self.resetTimeout, self.timeRecognition)
+            ret1, frame1 = self.vid1.get_frame_termal(self.pwidth, self.pheight)
+            if ret:
+                self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
+                self.canvas.create_image(0, 0, image=self.photo, anchor=tkinter.NW)
+            if ret1:
+                self.photo1 = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame1))
+                self.canvas1.create_image(0, 0, image=self.photo1, anchor=tkinter.NW)
+        else:
+            ret, frame, self.resetTimeout, self.timeRecognition = self.vid.get_frame_out(self.pwidth, self.pheight, self.resetTimeout, self.timeRecognition)
+            ret1, frame1 = self.vid1.get_frame_termal(self.pwidth, self.pheight)
+            if ret:
+                self.canvas.create_image(0, 0, image=self.photo, anchor=tkinter.NW)
+            if ret1:
+                self.canvas1.create_image(0, 0, image=self.photo1, anchor=tkinter.NW)
+            self.resetTimeout = self.resetTimeout - 1
         self.window.after(self.delay, self.update)
 
 
@@ -64,10 +78,22 @@ class MyVideoCapture:
             if not self.vid1.isOpened():
                 raise ValueError("Unable to open video source", idInputTermal)
 
-    def get_frame(self, pwidth, pheight):
+    def get_frame_out(self, pwidth, pheight, resetTimeout, timeRecognition):
         if self.vid.isOpened():
             ret, frame = self.vid.read()
-            #frame = cv2.flip(frame,1)
+            if ret:
+                frame = cv2.flip(frame, 1)
+                cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                return ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), resetTimeout, timeRecognition
+            else:
+                return ret, None, resetTimeout, timeRecognition
+        else:
+            return None, None, None, None
+
+    def get_frame(self, pwidth, pheight, resetTimeout, timeRecognition):
+        if self.vid.isOpened():
+            ret, frame = self.vid.read()
+            frame = cv2.flip(frame, 1)
             dim = (pwidth, pheight)
             # resize image
             frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
@@ -75,58 +101,101 @@ class MyVideoCapture:
                 # Return a boolean success flag and the current frame converted to BGR
                 # GRAY FRAME
                 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                p_x = int(pwidth / 2)
+                p_y = int(pheight / 2)
+                px = int(p_x / 2)
+                py = int(p_y / 2)
+                pw = (px + px)
+                ph = (py + py) + 50
+
+                # DRAW RECT IN FACE
+                cv2.rectangle(frame, (px, py), (px + pw, py + ph), (0, 255, 0), 2)
+                gray = gray[py:py + ph, px:px + pw]
+
                 # DETECT FACES IN FRAME
                 faces = face_cascade.detectMultiScale(gray, 1.4, 5)
 
-                # RUN ALL FACES IN FRAME
-                for i, (x, y, w, h) in enumerate(faces):
+                if len(faces) <= 1:
+                    # RUN ALL FACES IN FRAME
+                    for i, (x, y, w, h) in enumerate(faces):
 
-                    # DRAW RECT IN FACE
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                    position_faces.append(str(x) + "," + str(y) + "," + str(w) + "," + str(h))
+                        if timeRecognition > 0:
+                            timeRecognition = timeRecognition - 1
+                            continue
+                        # FACE CROP
+                        roi_gray = gray[y:y + h, x:x + w]
+                        detected_face = frame[int(y):int(y + h), int(x):int(x + w)]  # crop detected face
 
-                    # FACE CROP
-                    roi_gray = gray[y:y + h, x:x + w]
-                    detected_face = frame[int(y):int(y + h), int(x):int(x + w)]  # crop detected face
+                        if not detected_face.any():
+                            continue
 
-                    if not detected_face.any():
-                        continue
+                        # RESIZE FACE CROP TO 48x48
+                        resize = cv2.resize(roi_gray, (48, 48))
 
-                    # RESIZE FACE CROP TO 48x48
-                    resize = cv2.resize(roi_gray, (48, 48))
-
-                    # Indentificação da Pessoa #################################################
-                    idf, confidence = recognizer.predict(resize)
-                    nameP = "Desconhecido"
-
-                    if confidence > 150:
-                        nameP = label_faces[idf - 1].rstrip('\n')
-                    else:
+                        pName = ""
+                        # Indentificação da Pessoa #################################################
                         idf = "undefine"
+                        if faceRecognition:
+                            idf, confidence = recognizer.predict(resize)
+                            if confidence <= 130:
+                                idf = "undefine"
+                            else:
+                                nameP = label_faces[idf - 1].rstrip('\n')
 
-                    cv2.rectangle(frame, (x, y - 22), (x + w, y), (0, 255, 0), -1)
-                    cv2.putText(frame, nameP, (int(x), int(y) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1,
-                                cv2.LINE_AA)
+                        termalText = "38.0"
+                        try:
+                            if termalText != "undefine":
+                                valor = float(termalText)
+                                if faceRecognition:
+                                    if idf != "undefine":
+                                        if valor >= temperaturaCritica:
+                                            mixer.music.load('resource/sound/beep.mp3')
+                                            mixer.music.play()
+                                            self.text_to_speech("Dirigir-se à sala de aferição!")
+                                            resetTimeout = 5
+                                            timeRecognition = 5
+                                        else:
+                                            mixer.music.load('resource/sound/ok.mp3')
+                                            mixer.music.play()
+                                            self.speech_saudacao(nameP)
+                                            resetTimeout = 5
+                                            timeRecognition = 5
+                                        self.sendFace(idf, idOrigem, termalText, resize)
+                                    else:
+                                        mixer.music.load('resource/sound/beep.mp3')
+                                        mixer.music.play()
+                                        self.text_to_speech("Dirigir-se à sala de cadastro!")
+                                        resetTimeout = 5
+                                        timeRecognition = 5
+                                else:
+                                    nameP = "Temperatura aferida "+str(termalText)
+                                    if valor >= temperaturaCritica:
+                                        mixer.music.load('resource/sound/beep.mp3')
+                                        mixer.music.play()
+                                        self.text_to_speech("Dirigir-se à sala de aferição!")
+                                        resetTimeout = 5
+                                        timeRecognition = 5
+                                    else:
+                                        mixer.music.load('resource/sound/ok.mp3')
+                                        mixer.music.play()
+                                        self.speech_saudacao("")
+                                        resetTimeout = 5
+                                        timeRecognition = 5
+                                    self.sendFace(idf, idOrigem, termalText, resize)
+                                time.sleep(5)
+                        except:
+                            termalText = "undefine"
+                else:
+                    self.text_to_speech("Uma pessoa por vez, por favor!")
+                    time.sleep(2)
+                    resetTimeout = 5
+                    timeRecognition = 5
 
-                    #self.vid1.get_frame_termal(self.pwidth, self.pheight)
-
-                    try:
-                        if termalText != "undefine":
-                            valor = float(termalText)
-                            if valor >= temperaturaCritica:
-                                mixer.music.play()
-                                time.sleep(2)
-                    except:
-                        termalText = "undefine"
-
-                    if idf != "undefine" and termalText != "undefine":
-                        self.vid.sendFace(self, idf, idOrigem, termalText, resize)
-
-                return ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                return ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), resetTimeout, timeRecognition
             else:
-                return ret, None
+                return ret, None, resetTimeout, timeRecognition
         else:
-            return None, None
+            return None, None, None, None
 
     def get_frame_termal(self, pwidth, pheight):
         if self.vid1.isOpened():
@@ -136,7 +205,6 @@ class MyVideoCapture:
             w = 45
             h = 20
             cropTermal = frame1[y:y + h, x:x + w]
-            cv2.rectangle(frame1, (x, y), (x + w, y + h), (0, 0, 255), 1)
             try:
                 custom_config = r'--oem 3 --psm 6'
                 termalText = pytesseract.image_to_string(cropTermal, config=custom_config)
@@ -147,17 +215,6 @@ class MyVideoCapture:
             # resize image
             frame1 = cv2.resize(frame1, dim, interpolation=cv2.INTER_AREA)
             if ret1:
-                for faces in position_faces:
-                    x, y, w, h = faces.split(",")
-                    xt = int(x) + 0 #10
-                    yt = int(y) + 80
-                    wt = int(w) + 45
-                    ht = int(h) + 45
-                    cv2.rectangle(frame1, (xt, yt), (xt + wt, yt + ht), (255, 255, 255), 2)
-                    cv2.rectangle(frame1, (xt, yt - 22), (xt + wt, yt), (255, 255, 255), -1)
-                    cv2.putText(frame1, termalText, (xt, yt - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1,
-                                cv2.LINE_AA)
-                position_faces.clear()
                 # Return a boolean success flag and the current frame converted to BGR
                 return ret1, cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB)
             else:
@@ -183,6 +240,42 @@ class MyVideoCapture:
             print(f'Other error occurred: {err}')
 
             # Release the video source when the object is destroyed
+
+    def speech_saudacao(self, pNome):
+        saudacao = "um bom dia!"
+        data_e_hora_atual = datetime.now()
+        hora24HH = data_e_hora_atual.strftime('%H')
+        if 12 <= int(hora24HH) < 18:
+            saudacao = "uma boa tarde!"
+        elif 18 <= int(hora24HH) < 24:
+            saudacao = "uma boa noite!"
+
+        if faceRecognition:
+            audio = "Bem-vindo " + pNome + "! A MV deseja a você " + saudacao
+        else:
+            audio = "Bem-vindo! A MV deseja a você " + saudacao
+
+        tts = gTTS(audio, lang='pt-br', slow=False, lang_check=False)
+        # Salva o arquivo de audio
+        tts.save('audio.mp3')
+        # Da play ao audio
+        mixer.init()
+        mixer.music.load('audio.mp3')
+        mixer.music.play()
+        os.remove("audio.mp3")
+        time.sleep(3)
+
+    def text_to_speech(self, texto):
+        audio = texto
+        tts = gTTS(audio, lang='pt-br', slow=False, lang_check=False)
+        # Salva o arquivo de audio
+        tts.save('audio.mp3')
+        # Da play ao audio
+        mixer.init()
+        mixer.music.load('audio.mp3')
+        mixer.music.play()
+        os.remove("audio.mp3")
+        time.sleep(3)
 
     def __del__(self):
         if self.vid.isOpened():
@@ -212,13 +305,13 @@ global face_cascade
 global termalText
 global recognizer
 global label_faces
-global position_faces
 global pUrl
 global pTimeout
 global idOrigem
 global temperaturaCritica
 global idInputWebcam
 global idInputTermal
+global faceRecognition
 
 mixer.init()
 mixer.music.load('resource/sound/beep.mp3')
@@ -230,11 +323,14 @@ idOrigem = config['APP_CONFIG']["idOrigem"]
 idInputWebcam = int(config['APP_CONFIG']["idInputWebcam"])
 idInputTermal = int(config['APP_CONFIG']["idInputTermal"])
 temperaturaCritica = float(config['APP_CONFIG']["temperaturaCrítica"])
+if config['APP_CONFIG']["faceRecognition"] == 'True' or config['APP_CONFIG']["faceRecognition"] == 'true':
+    faceRecognition = True
+else:
+     faceRecognition = False
 face_cascade = cv2.CascadeClassifier('resource/model/haarcascade_frontalface_default.xml')
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read("resource/model/model_face_recognition.xml")
 text_file = open("resource/model/label_face_recognition.txt", "r")
 label_faces = text_file.readlines()
-position_faces = []
 termalText = ""
-App(tkinter.Tk(), "Recognition MV",int(config['APP_CONFIG']["pwidth"]),int(config['APP_CONFIG']["pheight"]))
+App(tkinter.Tk(), "Recognition MV", int(config['APP_CONFIG']["pwidth"]), int(config['APP_CONFIG']["pheight"]))
